85daa18baa32f144ba106a2a26b03fcb6949efba
jvogit <justinxvopro@gmail.com>
2020-10-05 00:22:45 -0700

    add: react controller

--- src/main/java/com/github/jvogit/bfit/api/AccountsApi.java
+++ src/main/java/com/github/jvogit/bfit/api/AccountsApi.java
@@ -1,13 +1,12 @@
 package com.github.jvogit.bfit.api;
 
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AccountsApi {
-    @GetMapping("/accounts")
+    @GetMapping("/register")
     public void getAccounts() {
-        LoggerFactory.getLogger(getClass()).info("Hello, world!");
+        
     }
 }
